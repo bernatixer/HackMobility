55d59d4943b012e136b9e5568c84582da34def8c
Sanchyy <sanchez.gassull@gmail.com>
2018-03-10 13:34:07 +0100

    merge solved

--- public/functions.js
+++ public/functions.js
@@ -11,15 +11,6 @@ var xhr = new XMLHttpRequest({mozSystem: true});
     $(document).ready(getJSON); 
 })(jQuery);
 
-
-
-=======
-$.getJSON(url, function(data) {
-    console.log(data)
-    loadPoints(data);
-});
->>>>>>> 401326ecceba5581df20305adaa404175a19f30f
-
 var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
 	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
